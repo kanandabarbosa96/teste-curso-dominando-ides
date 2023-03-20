d8eca85f946a7574709da685d6387ea287f30be2
kanandabarbosa <kanandabarbosa96@gmail.com>
2023-03-20 02:59:38 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,15 +2,49 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
-
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
 
+        Livro livro = new Livro("O Problema dos 3 corpos", 300);
+        System.out.println(livro);
         /*int a = 5;
         int b = 3;
         System.out.println("Hello word!" + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer npaginas;
+
+    public Livro(String nome, Integer npaginas) {
+        this.nome = nome;
+        this.npaginas = npaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpaginas() {
+        return npaginas;
+    }
+
+    public void setNpaginas(Integer npaginas) {
+        this.npaginas = npaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npaginas=" + npaginas +
+                '}';
     }
 }
